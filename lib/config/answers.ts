fa70ec20f917deff77ff4b0483afdebf4d42d03d
mapokapo <leopetrovic11@gmail.com>
2025-04-22 19:00:30 +0200

    fix: popravljen odgovor na 5. pitanje i dodano jos hintova za njega

--- lib/config/answers.ts
+++ lib/config/answers.ts
@@ -7,7 +7,7 @@
  * 1. Find hidden base64 string in HTML source code a website, decode it, read out the pixel values drawn inside the image, decode the binary data into ASCII text.
  * 2. Download an audio file, extract the Morse code signals from the audio, decode the Morse code into ASCII text.
  * 3. Transcribe DNA sequence into mRNA, then translate the mRNA sequence into a protein sequence using a lookup table.
- * 4. Solve a riddle to find the Vigenere cipher key, use the key to decrypt the ciphertext, use the Google Maps plus code in the plaintext to find the location, and the name of the location is the answer.
+ * 4. Solve a riddle to find the Vigenere cipher key, use the key to decrypt the ciphertext on the website, use the location name in the plaintext to find the Google Maps plus code for it, which is the answer.
  * 5. Recognize the pattern drawn on an HTML canvas, reverse engineer the JS code which draws the pattern, find the unused function in the code, input the name of the drawn pattern into the function, and the output is the answer.
  * 6. Play a numbers game with a bot inside of a web terminal emulator, upon winning the game the bot will reveal the answer.
  */
@@ -16,14 +16,5 @@ export const answers = [
   ["AUDITORYCORTEX", "AUDITORY CORTEX"],
   "TINKER",
   ["8RXJ+XC", "8RXJ+W7P", "8RXH+WXP"],
-  [
-    "SIERPINSKI",
-    "SIERPINSKI TRIANGLE",
-    "TROKUT SIERPINSKOG",
-    "sierpiński",
-    "sierpiński triangle",
-    "trokut sierpińskog",
-    "FRACTAL",
-    "FRAKTAL",
-  ],
+  "2025",
 ];
